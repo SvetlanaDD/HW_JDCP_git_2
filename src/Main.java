5e3012b24f8423156d9e9722de502e2c9b2643ea
SvetlanaDD <asweet.lana@gmail.com>
2022-07-19 00:48:15 +0300

    Branch longer - int to long

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main (String [] args){
-        int [] sales = {3, 5, 7, 1, 8, 2, 1};
+        long [] sales = {3, 5, 7, 1, 8, 2, 1};
         SalesManager example = new SalesManager(sales);
         System.out.println("Максимальные продажи - " + example.max());
     }
